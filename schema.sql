fd5e7f9f397a6a06036f2708de259c25113186ac
Jarrell Houston <jhouston2882@yahoo.com>
2021-03-18 21:03:55 -0500

    created database schema, api routes, partial ejs files, css folder structure

---
+++ schema.sql
@@ -0,0 +1,41 @@
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    ID SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+DROP TABLE IF EXISTS favorites;
+CREATE TABLE favorites (
+    ID SERIAL PRIMARY KEY,
+    favorites TEXT,
+    user_id INT NOT NULL,
+    CONSTRAINT fk_favorite_user 
+        FOREIGN KEY (user_id)
+            REFERENCES users (ID) 
+            ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS custom_sets;
+
+CREATE TABLE custom_sets (
+    ID SERIAL PRIMARY KEY,
+    words TEXT,
+    user_id INT NOT NULL,
+     CONSTRAINT fk_custom_sets_user 
+        FOREIGN KEY (user_id)
+            REFERENCES users (ID) 
+            ON DELETE CASCADE
+);
+
+-- INSERT INTO users(username, email, password) VALUES ('Jarrell28', 'jhouston2882@gmail.com', 'password');
+-- INSERT INTO users(username, email, password) VALUES ('Jarrell', 'jhouston2882@gmail.com', 'password');
+
+-- INSERT INTO favorites (favorites, user_id) VALUES ('word1,word3', 2);
+
+-- INSERT INTO custom_sets (words, user_id) VALUES ('word1,word2', 1);
+
+-- SELECT * FROM favorites WHERE user_id = 1;
+
+-- SELECT * FROM users INNER JOIN favorites ON users.id = favorites.user_id;
--- server.js
+++ server.js
@@ -1,22 +1,131 @@
 require("dotenv").config();
 
 const express = require('express');
+const { Client } = require('pg');
+const axios = require('axios');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 //Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname + '/public'));
 
 //Template Engine
 app.set('view engine', 'ejs');
 
+//Connecting to Database
+const client = new Client();
+client.connect(err => {
+    if (err) {
+        console.error('connection error', err.stack);
+    } else {
+        console.log('connected');
+    }
+});
 
 app.get('/', (request, response) => {
     response.render('index', { foo: 'FOO' });
 })
 
+//Gets All Users
+app.get('/users', (req, res) => {
+    client.query('SELECT * FROM users', (err, results) => {
+        if (err) res.status(400).json({ error: "Error occurred fetching users" });
+
+        res.status(200).json(results.rows);
+    })
+})
+
+//Get User by ID
+app.get('/user/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+
+    client.query('SELECT * FROM users WHERE id = $1', [id], (err, results) => {
+        if (err) res.status(400).json({ error: "Error occurred fetching users" });
+
+        res.status(200).json(results.rows);
+    })
+})
+
+//Gets All Favorites
+app.get('/favorites', (req, res) => {
+    client.query('SELECT * FROM favorites', (err, results) => {
+        if (err) res.status(400).json({ error: "Error occurred fetching favorites" });
+
+        res.status(200).json(results.rows);
+    })
+})
+
+//Get Favorites for one User
+app.get('/favorites/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+
+    client.query('SELECT * FROM favorites WHERE user_id = $1', [id], (err, results) => {
+        if (err) res.status(400).json({ error: "Error occurred fetching favorites" });
+
+        res.status(200).json(results.rows);
+    })
+})
+
+//Gets All Sets
+app.get('/sets', (req, res) => {
+    client.query('SELECT * FROM custom_sets', (err, results) => {
+        if (err) res.status(400).json({ error: "Error occurred fetching sets" });
+
+        res.status(200).json(results.rows);
+    })
+})
+
+//Gets Sets for one User
+app.get('/sets/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+
+    client.query('SELECT * FROM custom_sets WHERE user_id = $1', [id], (err, results) => {
+        if (err) res.status(400).json({ error: "Error occurred fetching sets" });
+
+        res.status(200).json(results.rows);
+    })
+})
+
+//Fetch 10 Random Words
+app.get('/random', (req, res) => {
+
+    const words = [];
+    const promises = [];
+
+    const options = {
+        method: 'GET',
+        url: 'https://wordsapiv1.p.rapidapi.com/words/',
+        params: {
+            random: 'true',
+            frequencymin: '8',
+            letterPattern: '^[A-Za-z]+$',
+            syllablesMax: '3',
+            hasDetails: 'definitions'
+        },
+        headers: {
+            'x-rapidapi-key': process.env.APIKEY,
+            'x-rapidapi-host': 'wordsapiv1.p.rapidapi.com'
+        }
+    };
+
+    for (let i = 0; i < 10; i++) {
+        promises.push(
+            axios.request(options).then(function (response) {
+                words.push(response.data.word);
+            })
+        )
+    }
+
+    Promise.all(promises).then(() => res.status(200).json(words));
+
+})
+
+
 
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
 })
+
