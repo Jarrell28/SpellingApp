1c583380aa5b7797aff6d18b961b8666d64ed6a2
Jarrell Houston <jhouston2882@yahoo.com>
2021-03-20 13:51:12 -0500

    Finished game main functionality. Login/Signup features completed

---
+++ public/js/loginSignup.js
@@ -0,0 +1,63 @@
+//Login Form
+$("#login").on("submit", function (e) {
+    e.preventDefault();
+
+    const email = $("#email").val();
+    const password = $("#password").val();
+
+    if (!email) return alert("Email is required");
+    if (!password) return alert("Password is required");
+
+    const formData = {
+        email,
+        password
+    }
+
+    $.ajax({
+        type: "POST",
+        url: "/login",
+        data: formData
+    }).done(response => {
+        if (response.status === "success") {
+            window.location.href = "/";
+        } else if (response.status === "failure") {
+            alert(response.error);
+        } else {
+            alert("Something went wrong!");
+        }
+    })
+})
+
+//Signup Form
+
+$("#signup").on("submit", function (e) {
+    e.preventDefault();
+
+    const username = $("#username").val();
+    const email = $("#email").val();
+    const password = $("#password").val();
+
+    if (!username) return alert("Username is required");
+    if (!email) return alert("Email is required");
+    if (!password) return alert("Password is required");
+
+    const formData = {
+        username,
+        email,
+        password
+    }
+
+    $.ajax({
+        type: "POST",
+        url: "/signup",
+        data: formData
+    }).done(response => {
+        if (response.status === "success") {
+            window.location.href = "/";
+        } else if (response.status === "failed") {
+            alert(response.error);
+        } else {
+            alert("Something went wrong!");
+        }
+    })
+})
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require('express');
 const { Client } = require('pg');
 const axios = require('axios');
+const session = require('express-session');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,6 +12,7 @@ const port = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
+app.use(session({ secret: process.env.SESSIONSECRET, cookie: { maxAge: 60000 } }))
 
 //Template Engine
 app.set('view engine', 'ejs');
@@ -25,8 +27,82 @@ client.connect(err => {
     }
 });
 
+app.all("/*", function (req, res, next) {
+    if (req.session.user) {
+        res.locals.user = req.session.user;
+    }
+    next();
+})
+
 app.get('/', (request, response) => {
     response.render('index', { foo: 'FOO' });
+})
+
+app.get('/login', (request, response) => {
+    response.render('login', { foo: 'FOO' });
+})
+
+app.get('/signout', (request, response) => {
+    request.session.destroy();
+    response.locals.user = "";
+    response.render('index');
+})
+
+app.post('/login', (request, response) => {
+    const email = request.body.email;
+    const password = request.body.password;
+
+    client.query("SELECT * FROM users WHERE email = $1::text LIMIT 1", [email], (err, results) => {
+        if (err) return console.log(err);
+
+        if (!results.rows) {
+            response.json({ status: "failed", error: "That email does not exist!" })
+        } else {
+            const user = results.rows[0];
+            if (user.password !== password) {
+                response.json({ status: "failed", error: "The email/password combination does not exist!" })
+            } else {
+                request.session.user = user;
+
+                response.status(200).json({ status: "success", user })
+            }
+        }
+    });
+})
+
+app.get('/signup', (request, response) => {
+    response.render('signup', { foo: 'FOO' });
+})
+
+app.post('/signup', (request, response) => {
+    const username = request.body.username;
+    const email = request.body.email;
+    const password = request.body.password;
+
+    client.query("SELECT * FROM users WHERE email = $1::text LIMIT 1", [email], (err, results) => {
+        if (err) return console.log(err);
+
+        if (results.rows > 0) {
+            response.json({ status: "failed", error: "That email already exists!" })
+        } else {
+
+            client.query("INSERT INTO users (username, email, password) VALUES($1, $2, $3)", [username, email, password], (err, results) => {
+                if (err) return console.log(err);
+
+                client.query("SELECT * FROM users WHERE email = $1::text LIMIT 1", [email], (err, results) => {
+                    if (err) return console.log(err);
+
+                    if (!results.rows) {
+                        response.json({ status: "failure", error: "Something else went wrong" });
+                    } else {
+                        const user = results.rows[0];
+                        request.session.user = user;
+                        response.status(200).json({ status: "success" })
+                    }
+                })
+            })
+        }
+    });
 })
 
 //Gets All Users
@@ -49,18 +125,26 @@ app.get('/user/:id', (req, res) => {
     })
 })
 
-//Gets All Favorites
+//Gets Favorites for active user page
 app.get('/favorites', (req, res) => {
-    client.query('SELECT * FROM favorites', (err, results) => {
+    if (!req.session.user) {
+        return res.redirect("/login");
+    }
+    const id = req.session.user.id;
+
+    client.query('SELECT * FROM favorites WHERE user_id = $1', [id], (err, results) => {
         if (err) res.status(400).json({ error: "Error occurred fetching favorites" });
 
-        res.status(200).json(results.rows);
+        res.render('favorites', { favorites: res.rows })
     })
 })
 
-//Get Favorites for one User
-app.get('/favorites/:id', (req, res) => {
-    const id = parseInt(req.params.id);
+//Get Favorites for active user game
+app.get('/favorites/user', (req, res) => {
+    if (!req.session.user) {
+        return res.redirect("/login");
+    }
+    const id = req.session.user.id;
 
     client.query('SELECT * FROM favorites WHERE user_id = $1', [id], (err, results) => {
         if (err) res.status(400).json({ error: "Error occurred fetching favorites" });
@@ -69,18 +153,26 @@ app.get('/favorites/:id', (req, res) => {
     })
 })
 
-//Gets All Sets
-app.get('/sets', (req, res) => {
-    client.query('SELECT * FROM custom_sets', (err, results) => {
+//Gets Custom words for page
+app.get('/custom', (req, res) => {
+    if (!req.session.user) {
+        return res.redirect("/login");
+    }
+    const id = req.session.user.id;
+
+    client.query('SELECT * FROM custom_sets WHERE user_id = $1', [id], (err, results) => {
         if (err) res.status(400).json({ error: "Error occurred fetching sets" });
 
-        res.status(200).json(results.rows);
+        res.render('custom', { custom: results.rows })
     })
 })
 
-//Gets Sets for one User
-app.get('/sets/:id', (req, res) => {
-    const id = parseInt(req.params.id);
+//Gets Sets for active user
+app.get('/custom/user', (req, res) => {
+    if (!req.session.user) {
+        return res.redirect("/login");
+    }
+    const id = req.session.user.id;
 
     client.query('SELECT * FROM custom_sets WHERE user_id = $1', [id], (err, results) => {
         if (err) res.status(400).json({ error: "Error occurred fetching sets" });
